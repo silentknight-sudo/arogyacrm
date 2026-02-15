4464386924820ba06010f0b9cd0d02a8ef590428
VIVEK SHUKLA <myspyzone000@gmail.com>
2026-02-15 17:58:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3238704164-621f1",
+  "appId": "1:489102531522:web:15213d56dff8e512d8945b",
+  "apiKey": "AIzaSyDaU5VnfSFQLnT49wVxkH2zFVCt21KpPLs",
+  "authDomain": "studio-3238704164-621f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489102531522"
+};
